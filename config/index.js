6b247a8c853b5f38b5c101859c17c1bb337a4e37
asher <243485908@qq.com>
2018-04-09 17:32:53 +0800

    Content:1.连通后台

--- config/index.js
+++ config/index.js
@@ -6,15 +6,23 @@ const path = require('path')
 
 module.exports = {
   dev: {
-
+    env: require('./dev.env'),
     // Paths
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-    proxyTable: {},
+    proxyTable: {
+      '/api': {
+          target: "http://localhost:8080",
+          changeOrigin: true,
+          pathRewrite: {
+            '^/api': ''   //需要rewrite重写的,
+        }
+      }
+    },
 
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
-    port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
+    port: 9600, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: false,
     errorOverlay: true,
     notifyOnErrors: true,
--- src/components/Test.vue
+++ src/components/Test.vue
@@ -1,3 +1,25 @@
 <template>
+<section>
   <el-input></el-input>
+  <el-button @click="login">陈琴红是zz</el-button>
+</section>
 </template>
+<script>
+import axios from 'axios';
+export default {
+    methods:{
+        login() {
+            axios({
+                method: "post",
+                url: `/api/user/add`,
+                data: {
+                    name: "asher",
+                    password: "q24348590"
+                    }
+            }).then(res => {
+                console.log(res);
+            })
+        }
+    }
+}
+</script>
